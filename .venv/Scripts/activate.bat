c4b85eacf3ea51d216497dc3013162a82bae8d9c
xnnrmv <xnnrmv@gmail.com>
2024-12-13 11:16:56 +0500

    Commit qildim

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,19 +8,15 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-<<<<<<< HEAD
-set VIRTUAL_ENV=D:\KI_AXB\online_market\.venv
-=======
-set VIRTUAL_ENV=D:\DJANGO\online_market\.venv
->>>>>>> 4c0ccf2dd1d44a0d4c49fe961a611065d2189380
+set "VIRTUAL_ENV=C:\dj\14\online_market\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
 if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
-set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(.venv) %PROMPT%
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(.venv) %PROMPT%"
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -28,8 +24,8 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(.venv) 
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+set "VIRTUAL_ENV_PROMPT=.venv"
 
 :END
 if defined _OLD_CODEPAGE (
